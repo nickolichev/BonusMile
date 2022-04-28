c6de11cd53e23c38c7092173b97a59c4eeff08de
nickolichev@gmail.com <nickolichev@gmail.com>
2022-04-28 16:06:16 +0300

    BonusMileOne

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1_000; // цена билета
+        int amount = 20; // за оплату каждых 20 руб начисляется бонусная 1 миля
+        int mile = 1; // количество миль за каждые 20 руб оплаты
+
+        int bonus = (price / amount);
+
+        System.out.println("Вам начислено бонусных миль:" + bonus);
+    }
+
 }
