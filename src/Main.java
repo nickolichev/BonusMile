52ac2a5ad47be5eadf8ab17279e5c63b3cdf36b8
nickolichev@gmail.com <nickolichev@gmail.com>
2022-04-28 16:15:49 +0300

    PriceTicket

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 1_000; // цена билета
+        int price = 1_010; // цена билета
         int amount = 20; // за оплату каждых 20 руб начисляется бонусная 1 миля
         int mile = 1; // количество миль за каждые 20 руб оплаты
 
